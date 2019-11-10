a14921919d5811bc75032b9522a859425e52ed3f
John Callahan <jcallahan@acm.org>
2019-11-09 21:05:36 -0500

    cleaned up UI

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,4 +1,7 @@
 class PurchasesController < ApplicationController
+  before_action do 
+    redirect_to new_user_session_path unless current_user && current_user.admin
+  end
   before_action :set_purchase, only: [:show, :edit, :update, :destroy]
 
   # GET /purchases
