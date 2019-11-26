5bf5f72e88cbd8fb68a9139a9714e6b2e6367404
John Callahan <jcallahan@acm.org>
2019-11-26 09:33:26 -0500

    add description to permitted params

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -72,6 +72,6 @@ class PlansController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def plan_params
-      params.require(:plan).permit(:name, :product_id, :price)
+      params.require(:plan).permit(:name, :product_id, :price, :description)
     end
 end
