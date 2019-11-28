a1010dec810c5ee37ed8a55c038eb543f1be4595
John Callahan <jcallahan@acm.org>
2019-11-28 07:26:11 -0500

    styling emails

--- test/mailers/previews/devise/mailer_preview.rb
+++ test/mailers/previews/devise/mailer_preview.rb
@@ -3,13 +3,19 @@ class Devise::MailerPreview < ActionMailer::Preview
       Devise::Mailer.confirmation_instructions(User.first, {})
     end
    
-    # hit http://localhost:3000/rails/mailers/devise/mailer/reset_password_instructions
     def reset_password_instructions
-      Devise::Mailer.reset_password_instructions(User.first, {})
+        Devise::Mailer.reset_password_instructions(User.first, {})
     end
    
-    # hit http://localhost:3000/rails/mailers/devise/mailer/unlock_instructions
     def unlock_instructions
       Devise::Mailer.unlock_instructions(User.first, {})
+    end
+
+    def password_change
+        Devise::Mailer.password_change(User.first, {})
+    end
+
+    def email_change
+        Devise::Mailer.email_change(User.first, {})
     end
 end
